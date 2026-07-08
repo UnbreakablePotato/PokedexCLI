4ba43222b0ef1472aa7e290f28c29b1eafee6df7
UnbreakablePotato <swipeytwit@gmail.com>
2026-07-08 23:48:46 +0200

    Added caching

--- cli.go
+++ cli.go
@@ -7,7 +7,11 @@ import (
 	"io"
 	"net/http"
 	"os"
+
+	"github.com/UnbreakablePotato/pokedexcli/internal/pokecache"
 )
+
+const duration = 60000000000
 
 type cliCommand struct {
 	name        string
@@ -46,45 +50,83 @@ func generateUsage() {
 	}
 }
 
-var con = config{URL: "https://pokeapi.co/api/v2/location-area"}
+var cache = pokecache.NewCache(duration)
+
+var con = config{URL: "https://pokeapi.co/api/v2/location-area?offset=0&limit=20"}
 
 func getMap(c *config) error {
 
-	res, err := http.Get(con.URL)
-	if err != nil {
-		return err
+	keyStr := con.URL
+
+	fmt.Printf("Current con.URL: %s\n", keyStr)
+
+	val, b := cache.Get(keyStr)
+
+	fmt.Println("In map function...")
+
+	if b == true {
+		fmt.Println("debug: Found key in cache...")
+		if err := json.Unmarshal(val, &con); err != nil {
+			return err
+		}
+
+		fmt.Println("debug: No error when unmarshalling json...")
+
+		for i := 0; i < 20; i++ {
+			fmt.Printf("%v\n", con.Results[i].Name)
+		}
+
+		con.URL = con.Next
+
+		return nil
+	} else {
+		fmt.Println("debug: Not in cache, making GET request")
+		res, err := http.Get(con.URL)
+		fmt.Println("debug: Request successful...")
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			return err
+		}
+
+		defer res.Body.Close()
+
+		if res.StatusCode != http.StatusOK {
+			err := fmt.Sprintf("Status code:%v", res.StatusCode)
+			return errors.New(err)
+		}
+
+		data, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			return err
+		}
+
+		cache.Add(con.URL, data)
+
+		con = config{}
+
+		fmt.Println("debug: ReadAll sucessful")
+
+		if err := json.Unmarshal(data, &con); err != nil {
+			fmt.Printf("Error: %v\n", err)
+			return err
+		}
+
+		fmt.Println("debug: Unmarshal successful")
+
+		for i := 0; i < 20; i++ {
+			fmt.Printf("%v\n", con.Results[i].Name)
+		}
+
+		con.URL = con.Next
+
+		return nil
 	}
-
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusOK {
-		err := fmt.Sprintf("Status code:%v", res.StatusCode)
-		return errors.New(err)
-	}
-
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
-
-	con = config{}
-
-	if err := json.Unmarshal(data, &con); err != nil {
-		return err
-	}
-
-	for i := 0; i < 20; i++ {
-		fmt.Printf("%v\n", con.Results[i].Name)
-	}
-
-	con.URL = con.Next
-
-	return nil
 }
 
 func getPrevMap(c *config) error {
 
-	if con.Prev == "null" {
+	if con.Prev == "" {
 		fmt.Println("You're on the first page")
 		return nil
 	}
